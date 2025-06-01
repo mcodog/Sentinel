c257dbe71dae444425f6e2283dd61c127bf9e352
mcodog <markandrie.codog@tup.edu.ph>
2025-06-01 15:06:57 +0800

    analyze by session|next step: display to frontend

--- backend/routes/conversation.routes.js
+++ backend/routes/conversation.routes.js
@@ -3,13 +3,14 @@ import {
   generateResponse,
   initializeSession,
   retrieveConversations,
+  retrieveMessagesofSession,
 } from "../controllers/conversation.controller.js";
 
 const router = express.Router();
 
 router.post("/initialize", initializeSession);
 router.post("/", generateResponse);
-
 router.get("/:userId", retrieveConversations);
+router.get("/session/:sessionId", retrieveMessagesofSession);
 
 export default router;
